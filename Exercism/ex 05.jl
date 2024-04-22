1e23bd476b11c7ffa9394b3659afe535f6bdb193
Defendi <tiagodefendidasilva@gmail.com>
2024-04-22 17:02:20 -0300

    solve new exercises

---
+++ Exercism/ex 05.jl
@@ -0,0 +1,41 @@
+#=
+Introduction
+Raindrops is a slightly more complex version of the FizzBuzz challenge, a classic interview question.
+
+Instructions
+Your task is to convert a number into its corresponding raindrop sounds.
+
+If a given number:
+
+is divisible by 3, add "Pling" to the result.
+is divisible by 5, add "Plang" to the result.
+is divisible by 7, add "Plong" to the result.
+is not divisible by 3, 5, or 7, the result should be the number as a string.
+Examples
+28 is divisible by 7, but not 3 or 5, so the result would be "Plong".
+30 is divisible by 3 and 5, but not 7, so the result would be "PlingPlang".
+34 is not divisible by 3, 5, or 7, so the result would be "34".
+=#
+
+raindrops = function(number)
+    string = ""
+    if number%3 == 0
+        string *= "Pling"
+    end
+    if number%5 == 0
+        string *= "Plang"
+    end
+    if number%7 == 0
+        string *= "Plong"
+    end
+    if string == ""
+        string *= "$number"
+    end
+
+    return string
+end
+
+main = function ()
+    println(raindrops(15))
+end
+main()
